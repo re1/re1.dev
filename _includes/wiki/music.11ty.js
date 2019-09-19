a54f0603fd134404585f1e017ea6d6943e7ca465
Markus Reichl <markus@re1.at>
2019-09-19 21:18:42 +0200

    Create my first wiki page for music special enough for me to share
    Wiki page will be linked later as it grows.

---
+++ _includes/wiki/music.11ty.js
@@ -0,0 +1,24 @@
+module.exports = class Music {
+  data() {
+    return {
+      layout: 'layout',
+    }
+  }
+
+  render({ content, songs }) {
+    return /*html*/ `
+      ${content}
+      ${songs
+        .map(
+          song => `
+            <h2>${song.title}</h2>
+            <p>${song.artist}</p>
+            ${song.note ? `<p>${song.note}</p>` : ''}
+            ${song.spotify ? `<a href="${song.spotify}" rel="external" title="${song.title} on Spotify">Spotify</a>` : ''}
+            ${song.youtube ? `<a href="${song.youtube}" rel="external" title="${song.title} on YouTube">YouTube</a>` : ''}
+          `
+        )
+        .join('')}
+    `
+  }
+}
