6c0b6618ab3d3820d8a413a6b15f65e0936e28eb
Markus Reichl <markus@re1.at>
2022-04-03 14:11:30 +0200

    Upgrade packages and use recursive from Fontsource instead of local files

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,24 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 const htmlmin = require('html-minifier')
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats('11ty.js,md')
   eleventyConfig.addPassthroughCopy('static')
+  // add recursive font from Fontsource
+  eleventyConfig.addPassthroughCopy({
+    'node_modules/@fontsource/recursive/files/recursive-latin-variable-full-normal.woff2':
+      'static/fonts/recursive.woff2',
+  })
   // add syntax highlighting plugin
   eleventyConfig.addPlugin(syntaxHighlight)
   // minify html using html-minifier
-  eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
     if (outputPath.endsWith('.html')) {
-      let minified = htmlmin.minify(content, {
+      return htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true,
       })
-      return minified
     }
 
     return content
